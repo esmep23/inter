ec765816623f3b23f76c5c06f06b3131bae47c41
esmep23 <stefipulgar@gmail.com>
2016-12-30 14:55:18 -0500

    5-30_12_2016

--- js/interlab.js
+++ js/interlab.js
@@ -169,7 +169,7 @@ myApp.onPageBeforeInit('index', function (page) {
     $( ".consultoPdf" ).click(function() {
       alert(0);
       //window.open('http://interlab.com.ec/app/movil/pdf.php?arg0=7777111&arg1=21099',"_blank");
-      window.location="http://interlab.com.ec/app/movil/pdf.php?arg0=7777111&arg1=21099";
+      location.href="http://interlab.com.ec/app/movil/pdf.php?arg0=7777111&arg1=21099";
       
 /*
       $.ajax({
